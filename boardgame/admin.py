6f52486dac48b99d0d1346d2eca4c5a47000f4f5
Aramis Magomedov <aramis.magomedov@mail.ru>
2026-03-31 13:52:57 +0300

    добавил модель в админку

--- boardgame/admin.py
+++ boardgame/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Boardgame
+
 # Register your models here.
+@admin.register(Boardgame)
+class BoardgameAdmin(admin.ModelAdmin):
+    list_display = ("title", "artist")
+
+
